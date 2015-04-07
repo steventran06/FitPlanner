780cb130a9494a82da446a2eb5258d0be2ef99f2
steventran06 <steventran06@gmail.com>
2015-04-06 22:08:13 -0700

    Added Devise authentification

--- StevenTran-steventran06/Ritly/app/models/url.rb
+++ StevenTran-steventran06/Ritly/app/models/url.rb
@@ -1,2 +1,4 @@
 class Url < ActiveRecord::Base
+	validates :link, presence: true
+	# validates :link, format: {with: /www.+/, message: "Please begin with www." }
 end
--- StevenTran-steventran06/Ritly/config/routes.rb
+++ StevenTran-steventran06/Ritly/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'urls/create'
   # get 'urls/new'
   # get 'urls/show'
